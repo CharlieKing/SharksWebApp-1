0155aecb5f5619f976e49d05ea6b3c166fa83bb6
jvilla93 <villapx@yahoo.com>
2013-11-22 12:39:58 -0500

    initial commit

---
+++ application/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SharksApp::Application.config.secret_key_base = '0410a092071ac0f4a8cc0345839db1e54850b005494db24a5375caeb2d0582fef591f4a737109433c8a94c5ebbedf0122c577c2db108b05e09f319b540dcdb8c'
